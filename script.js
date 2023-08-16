7470c45855671b36485d319fa9bcc1aafda9dc05
Alan Alvarado <alvarado.aac@gmail.com>
2023-08-16 07:15:42 -0600

    Tarea, vere la forma de terminrlo en la compu del trabo

---
+++ script.js
@@ -0,0 +1,51 @@
+/*
+Ejercicios if-else:
+
+1.Solicitar al usuario que responda a la pregunta (“¿Eres bellisimo/a?”), en caso de contestar sí, responder “Ciertamente”, en caso de contestar no, responder: “No te creo”.
+*/
+
+function eresBellisimo() {
+    let pregunta = prompt("¿Eres bellisimo/a?");
+
+    if (pregunta = "si") {
+        document.write("Ciertamente");
+    } else (pregunta = "no"){
+        document.write("No te creo");
+    }
+}
+
+eresBellisimo();
+
+/*
+2.Solicitar al usuario un número, y determinar si es divisible entre dos o no. Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm
+*/
+
+/*
+3.Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert.
+*/
+
+/*
+BECA
+Un conocido portal de educación en tecnología está ofreciendo programas para aprender a desarrollar aplicaciones. Escribe un programa que le indique a la persona interesada cuánto deberá pagar mensualmente de acuerdo a la opción elegida.
+El programa educativo contempla 3 diferentes niveles, cada uno con su costo mensual:
+Course: $4999 MXN
+Carrera $3999 MXN
+Master: $2999 MXN
+Adicionalmente preguntar si cuenta con alguna beca y aplicar el descuento correspondiente al precio final.
+Beca Facebook: 20% de descuento.
+Beca Google: 15% de descuento.
+Beca Jesua: 50% de descuento.
+Finalmente, además del precio mensual con descuento, indicar al usuario cuánto gastaría en total por el curso elegido, tomando en cuenta las siguientes duraciones:
+Course: 2 meses
+Carrera 6 meses
+Master: 12 meses
+*/
+
+
+/*
+CARRO
+Realizar un programa que ayude a calcular el total a pagar de acuerdo a la distancia recorrida por un vehículo con cargo extra por los litros consumidos, tomando en consideración lo siguiente:
+Si el vehículo es “coche”, el precio kilometro ha de ser 0.20, si es “moto” ha de ser 0.10 y si es “autobús” 0.5.
+Si los litros consumidos están entre 0 y 100 se ha de añadir 5 al costo total, si es mayor la cantidad de litros consumidos se ha de añadir 10 al total. Considere qué:
+total a pagar = (precio kilometro x kms recorridos) + extra por litros consumidos.
+*/
